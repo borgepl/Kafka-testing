4b84433a0490b2186f5585fade6e630fc98de5b4
borgepl <pdeborger@gmail.com>
2021-04-22 16:38:32 +0200

    Added a Kafka Consumer

--- src/main/java/com/deborger/kafka/ConsumerDemo.java
+++ src/main/java/com/deborger/kafka/ConsumerDemo.java
@@ -1,10 +1,16 @@
 package com.deborger.kafka;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Properties;
 
 public class ConsumerDemo {
@@ -16,6 +22,7 @@ public class ConsumerDemo {
         String bootstrapServers = "s-eacea-testpdb:9092";
         String groupId = "my-test-application";
         String offsetReset = "earliest";
+        String topic = "firsttopic";
 
         Logger logger = LoggerFactory.getLogger(ConsumerDemo.class);
 
@@ -28,7 +35,20 @@ public class ConsumerDemo {
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,offsetReset);
 
         // create the consumer
+        KafkaConsumer<String,String> consumer =
+                new KafkaConsumer<String, String>(properties);
 
-        
+        // Subscribe Consumer to topic
+        // consumer.subscribe(Collections.singleton(topic)); // only to one topic
+        consumer.subscribe(Arrays.asList(topic)); // subscribe to multiple topics
+
+        // poll for new data
+        while (true) {
+            ConsumerRecords<String,String> records = consumer.poll(Duration.ofMillis(100));
+
+            for (ConsumerRecord<String,String> record : records) {
+                logger.info("Key : " + record.key() + "- Value : " + record.value());
+            }
+        }
     }
 }
